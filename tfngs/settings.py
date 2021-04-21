aeff1ba5e49f6f6d8dd5bd5c852be4665a3bba50
kr1tsurf3r <atwenzel49@gmail.com>
2021-04-21 15:56:46 -0700

    removed secret key and reset

--- tfngs/settings.py
+++ tfngs/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5ak%1k^e0pbogb+mgmchqrtn#$$h9*l%al=$0bblvtybtg))h('
+#SECRET_KEY = 'no'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
