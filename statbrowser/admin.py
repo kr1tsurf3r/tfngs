e1195111bfe2db8cb04297b976275147a3af9f43
atwenzel <atwenzel@eng.ucsd.edu>
2023-12-25 12:49:56 -0800

    repo reset 12/2023

--- statbrowser/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Player, MatchPlayer, Match 
-
-admin.site.register(Match)
--- statbrowser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatbrowserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'statbrowser'
